f71c758c632cb6a0def62aba1d284a99e18257d4
TERESIA012 <kingoriteresia@gmail.com>
2021-08-20 00:07:21 +0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Goal } from './goals';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Goals-Teresia';
+  goals:string[];
+
+}
